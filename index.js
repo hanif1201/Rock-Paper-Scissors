ccdb9fa2f5713202584ee4d31ab731237cbf7414
hanifdev <adeyemihanif@gmail.com>
2022-07-26 02:26:43 +0100

    game script updated

---
+++ index.js
@@ -0,0 +1,17 @@
+'strict mode'
+
+myArray = ['rock', 'paper', 'scissors']
+
+function getComputersChoice(){
+
+    return myArray[Math.floor(Math.random() * myArray.length)];
+
+    
+
+}
+
+getComputersChoice()
+
+function playGame(playerSelection , computerSelection ){
+    if
+}
