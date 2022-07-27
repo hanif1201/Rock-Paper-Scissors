71406ea428cebaad98c63e7c89bcc9b561fbe78f
hanifdev <adeyemihanif@gmail.com>
2022-07-27 12:10:32 +0100

    js code update

--- index.js
+++ index.js
@@ -10,8 +10,31 @@ function getComputersChoice(){
 
 }
 
-getComputersChoice()
+const playerSelection = "rock";
+const computerSelection = getComputersChoice();
 
-function playGame(playerSelection , computerSelection ){
-    if
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if(playerSelection === computerSelection){
+        console.log('There is a Tie')
+    }else if (computerSelection == 'rock' && playerSelection == 'paper'  || 
+        computerSelection == 'scissors' && playerSelection == 'rock' || 
+        computerSelection == 'paper' && playerSelection == 'scissors'){
+        console.log('Oh no!, you lost ' +  computerSelection + ' ' + 'beats ' + playerSelection)
+    }else if(playerSelection == 'rock' && computerSelection == 'paper'  || 
+    playerSelection == 'scissors' && computerSelection == 'rock' || 
+    playerSelection == 'paper' && computerSelection == 'scissors'){
+    console.log('Congratulations!, You win ' +  playerSelection + ' ' + 'beats ' + computerSelection)
+    }
+    
 }
+   
+for (let i = 0; i < 5; i++) {
+    // your code here!
+
+    playRound(playerSelection, computerSelection);
+}
+  
+
+
+
