33afc01bc4f062214591ba42fa2fb09336813c55
hanifdev <adeyemihanif@gmail.com>
2022-07-27 23:02:20 +0100

    Script Updated

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-'strict mode'
+"strict mode";
 
-myArray = ['rock', 'paper', 'scissors']
+myArray = ["rock", "paper", "scissors"];
 
-function getComputersChoice(){
-
-    return myArray[Math.floor(Math.random() * myArray.length)];
-
-    
-
+function getComputersChoice() {
+  return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 const playerSelection = "paper";
 const computerSelection = getComputersChoice();
 
-console.log(computerSelection)
+console.log(computerSelection);
+console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    if(playerSelection === computerSelection){
-        console.log('There is a Tie')
-    }else if (computerSelection == 'rock' && playerSelection == 'paper'  || 
-        computerSelection == 'scissors' && playerSelection == 'rock' || 
-        computerSelection == 'paper' && playerSelection == 'scissors'){
-        console.log('Oh no!, you lost ' +  computerSelection + ' ' + 'beats ' + playerSelection)
-    }else if(playerSelection == 'rock' && computerSelection == 'paper'  || 
-    playerSelection == 'scissors' && computerSelection == 'rock' || 
-    playerSelection == 'paper' && computerSelection == 'scissors'){
-    console.log('Congratulations!, You win ' +  playerSelection + ' ' + 'beats ' + computerSelection)
-    }
-
-    
-    
+  // your code here!
+  if (playerSelection === computerSelection) {
+    console.log("There is a Tie");
+  } else if (
+    (computerSelection == "rock" && playerSelection == "paper") ||
+    (computerSelection == "scissors" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "rock")
+  ) {
+    console.log(
+      "Oh no!, you lost " + computerSelection + " " + "beats " + playerSelection
+    );
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "scissors" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissors")
+  ) {
+    console.log(
+      "Congratulations!, You win " +
+        playerSelection +
+        " " +
+        "beats " +
+        computerSelection
+    );
+  } else {
+    console.log("nah");
+  }
 }
+
+playRound(playerSelection, computerSelection);
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        // your code here!
-    
-        playRound(playerSelection, computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-    
-        
-    }
-      
-    
+  for (let i = 0; i < 5; i++) {
+    // your code here!
+
+    playRound(playerSelection, computerSelection);
+    console.log(game(playerSelection, computerSelection));
+  }
 }
-   
+
 game();
-
-
